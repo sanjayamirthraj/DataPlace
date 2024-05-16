c8d0770c77429db5438ac24dd0e1f62c9547a43f
Sanjay Amirthraj <sanjay.amirthraj@gmail.com>
2024-05-16 11:53:29 -0500

    tried running through one of the steps in the docs --> make sure to load in env variables

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,19 +6,4 @@ const nextConfig = {
 module.exports = nextConfig;
 
 
-import { custom } from 'viem';
-import { Address } from 'viem/accounts';
-import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
 
-if (window.ethereum) {
-  const [account]: [Address] = await window.ethereum.request({
-    method: "eth_requestAccounts",
-  });
-  
-  const config: StoryConfig = {
-    transport: custom(window.ethereum),
-    account: account, // the account address from above
-    chainId: 'sepolia'
-  };
-  export const client = StoryClient.newClient(config);
-}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,12 +2,27 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useAccount } from "wagmi";
+import { client } from "../config";
+
+const response = await client.ipAsset.register({
+  nftContract: "0xd516482bef63Ff19Ed40E4C6C2e626ccE04e19ED", // your NFT contract address
+  tokenId: "12", // your NFT token ID
+  txOptions: { waitForTransaction: true },
+});
+
+console.log(
+  `Root IPA created at transaction hash ${response.txHash}, IPA ID: ${response.ipId}`
+);
 
 const Home: NextPage = () => {
+  const account = useAccount();
+
   return (
     <div className={styles.container}>
       <title>Story Protocol Marketplace</title>
-      <p className="bg-red-100">Story Protocol Marketplace</p>
+      <ConnectButton />
+      <p className="bg-red-100">Story Protocol Marketplace {account.address}</p>
     </div>
   );
 };
