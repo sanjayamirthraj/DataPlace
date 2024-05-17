2ada9efa3c6d5433032c624c16c223b439a5b56c
Sanjay Amirthraj <sanjay.amirthraj@gmail.com>
2024-05-17 12:21:56 -0500

    Minted NFT with link dont really understand how to mint and register --> have to work on that

--- frontend/config.tsx
+++ frontend/config.tsx
@@ -1,9 +1,10 @@
-import { http } from "viem";
 import { Account, privateKeyToAccount, Address } from "viem/accounts";
 import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
+import { http, createWalletClient, createPublicClient } from "viem";
+import { sepolia } from "viem/chains";
 
 const privateKey: Address = `0x2fa8efe237294d598f4c2699f69a0a9228c5263805a408dffabbea6dcf6e4105`;
-const account: Account = privateKeyToAccount(privateKey);
+export const account: Account = privateKeyToAccount(privateKey);
 
 const config: StoryConfig = {
   transport: http(process.env.RPC_PROVIDER_URL),
@@ -12,3 +13,13 @@ const config: StoryConfig = {
 };
 
 export const client = StoryClient.newClient(config);
+
+export const walletClient = createWalletClient({
+  account,
+  chain: sepolia,
+  transport: http("https://rpc.ankr.com/eth_sepolia"),
+});
+export const publicClient = createPublicClient({
+  chain: sepolia,
+  transport: http("https://rpc.ankr.com/eth_sepolia"),
+});
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,14 +3,20 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useAccount } from "wagmi";
-import { client } from "../config";
+import { account, client } from "../config";
+import { Account, privateKeyToAccount, Address } from "viem/accounts";
+import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
+import { http, createWalletClient, createPublicClient } from "viem";
+import { sepolia } from "viem/chains";
+import Link from "next/link";
+
 //general workflow --> register NFT, register IPA (can do both at once as well potentially), attach license terms, place on marketplace
 
 //undefined for txHash cuz already registered
 //this is how to register the IPA
 const response = await client.ipAsset.register({
-  nftContract: "0xd516482bef63Ff19Ed40E4C6C2e626ccE04e19ED", // your NFT contract address
-  tokenId: "12", // your NFT token ID
+  nftContract: "0x0f00a58A741aD6C9DFb549e8B0aad1e9bC48D9f1", // your NFT contract address
+  tokenId: "44", // your NFT token ID
   txOptions: { waitForTransaction: true },
 });
 
@@ -54,6 +60,14 @@ const Home: NextPage = () => {
         ID: 0x234B4a0bBC330598aCe669616B9Eff465bd4F3ea -- this is because this
         transaction has already been registered (taken from docs)
       </p>
+      <Link
+        href={
+          "https://explorer.storyprotocol.xyz/ipa/0x7241771200d2Fdf8a7a5ffb023d651e288C7940b"
+        }
+        className="bg-green-100 m-6 p-6 flex justify-center"
+      >
+        Minted NFT (Click to be directed)
+      </Link>
     </div>
   );
 };
